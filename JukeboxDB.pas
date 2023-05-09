f216ef9bc96f44f3d17966fcf96572045af1f28f
Paul Dardeau <pauldardeau@gmail.com>
2023-05-09 18:42:59 -0500

    add missing function skeletons

--- JukeboxDB.pas
+++ JukeboxDB.pas
@@ -33,6 +33,8 @@ type
     function BeginTransaction: Boolean;
     function Rollback: Boolean;
     function Commit: Boolean;
+    function CreateTable(SqlStatement: String): Boolean;
+    function CreateTables: Boolean;
     function HaveTables: Boolean;
     function GetPlaylist(PlaylistName: String): String;
     function SongsForArtist(ArtistName: String): TListSongMetadata;
@@ -43,8 +45,16 @@ type
     procedure ShowGenres;
     procedure ShowAlbums;
     procedure ShowPlaylists;
-	function SongsForQueryResults(QueryResults: TSQLQuery): TListSongMetadata;
-	function SqlWhereClause: String;
+    function SongsForQueryResults(QueryResults: TSQLQuery): TListSongMetadata;
+    function SqlWhereClause: String;
+    function InsertSong(Song: TSongMetadata): Boolean;
+    function UpdateSong(Song: TSongMetadata): Boolean;
+    function StoreSongMetadata(Song: TSongMetadata): Boolean;
+    function InsertPlaylist(PlUid: String;
+                            PlName: String;
+                            PlDesc: String): Boolean;
+    function DeleteSong(SongUid: String): Boolean;
+    function DeletePlaylist(PlName: String): Boolean;
   end;
 
 implementation
@@ -206,6 +216,92 @@ end;
 
 //*******************************************************************************
 
+function TJukeboxDB.CreateTable(SqlStatement: String): Boolean;
+var
+  DidSucceed: Boolean;
+begin
+  DidSucceed := false;
+
+  //TODO: implement CreateTable
+
+  CreateTable := DidSucceed;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.CreateTables: Boolean;
+var
+  DidSucceed: Boolean;
+  CreateGenreTable: String;
+  CreateArtistTable: String;
+  CreateAlbumTable: String;
+  CreateSongTable: String;
+  CreatePlaylistTable: String;
+  CreatePlaylistSongTable: String;
+begin
+  DidSucceed := false;
+
+  if DbConnection <> nil then begin
+    if DebugPrint then begin
+      writeLn('creating tables');
+    end;
+
+    CreateGenreTable := 'CREATE TABLE genre (' +
+                        'genre_uid TEXT UNIQUE NOT NULL, ' +
+                        'genre_name TEXT UNIQUE NOT NULL, ' +
+                        'genre_description TEXT);';
+
+    CreateArtistTable := 'CREATE TABLE artist (' +
+                         'artist_uid TEXT UNIQUE NOT NULL,' +
+                         'artist_name TEXT UNIQUE NOT NULL,' +
+                         'artist_description TEXT);';
+
+    CreateAlbumTable := 'CREATE TABLE album (' +
+                        'album_uid TEXT UNIQUE NOT NULL,' +
+                        'album_name TEXT UNIQUE NOT NULL,' +
+                        'album_description TEXT,' +
+                        'artist_uid TEXT NOT NULL REFERENCES artist(artist_uid),' +
+                        'genre_uid TEXT REFERENCES genre(genre_uid));';
+
+    CreateSongTable := 'CREATE TABLE song (' +
+                       'song_uid TEXT UNIQUE NOT NULL,' +
+                       'file_time TEXT,' +
+                       'origin_file_size INTEGER,' +
+                       'stored_file_size INTEGER,' +
+                       'pad_char_count INTEGER,' +
+                       'artist_name TEXT,' +
+                       'artist_uid TEXT REFERENCES artist(artist_uid),' +
+                       'song_name TEXT NOT NULL,' +
+                       'md5_hash TEXT NOT NULL,' +
+                       'compressed INTEGER,' +
+                       'encrypted INTEGER,' +
+                       'container_name TEXT NOT NULL,' +
+                       'object_name TEXT NOT NULL,' +
+                       'album_uid TEXT REFERENCES album(album_uid));';
+
+    CreatePlaylistTable := 'CREATE TABLE playlist (' +
+                           'playlist_uid TEXT UNIQUE NOT NULL,' +
+                           'playlist_name TEXT UNIQUE NOT NULL,' +
+                           'playlist_description TEXT);';
+
+    CreatePlaylistSongTable := 'CREATE TABLE playlist_song (' +
+                               'playlist_song_uid TEXT UNIQUE NOT NULL,' +
+                               'playlist_uid TEXT NOT NULL REFERENCES playlist(playlist_uid),' +
+                               'song_uid TEXT NOT NULL REFERENCES song(song_uid));';
+
+    DidSucceed := CreateTable(CreateGenreTable) and
+                  CreateTable(CreateArtistTable) and
+                  CreateTable(CreateAlbumTable) and
+                  CreateTable(CreateSongTable) and
+                  CreateTable(CreatePlaylistTable) and
+                  CreateTable(CreatePlaylistSongTable);
+  end;
+
+  CreateTables := DidSucceed;
+end;
+
+//*******************************************************************************
+
 function TJukeboxDB.HaveTables: Boolean;
 var
   Sql: String;
@@ -301,7 +397,7 @@ begin
                 ' AND artist = :artist_name';
     
     DbQuery.SQL.Text := SqlQuery;
-	DbQuery.Params.ParamByName('artist_name').AsString := ArtistName;
+    DbQuery.Params.ParamByName('artist_name').AsString := ArtistName;
     BeginTransaction;
     DbQuery.Open;
 
@@ -340,7 +436,7 @@ begin
                 'WHERE song_uid = :song_uid';
 
     DbQuery.SQL.Text := SqlQuery;
-	DbQuery.Params.ParamByName('song_uid').AsString := SongUid;
+    DbQuery.Params.ParamByName('song_uid').AsString := SongUid;
     BeginTransaction;
     DbQuery.Open;
     
@@ -388,19 +484,19 @@ begin
       if Album.Length > 0 then begin
         EncodedAlbum := EncodeValue(Album);
         AddedClause := ' AND object_name LIKE ' +
-		               STRING_OF_SINGLE_QUOTE +
-		               EncodedArtist +
-					   '--' +
-					   EncodedAlbum +
-					   '%' +
-					   STRING_OF_SINGLE_QUOTE;
+                       STRING_OF_SINGLE_QUOTE +
+                       EncodedArtist +
+                       '--' +
+                       EncodedAlbum +
+                       '%' +
+                       STRING_OF_SINGLE_QUOTE;
       end
       else begin
         AddedClause := ' AND object_name LIKE ' +
-		               STRING_OF_SINGLE_QUOTE +
-		               EncodedArtist +
-					   '--%' +
-					   STRING_OF_SINGLE_QUOTE;
+                       STRING_OF_SINGLE_QUOTE +
+                       EncodedArtist +
+                       '--%' +
+                       STRING_OF_SINGLE_QUOTE;
       end;
       SqlQuery := SqlQuery + AddedClause;
     end;
@@ -605,7 +701,7 @@ begin
         song.AlbumUid := QueryResults.Fields.Fields[13].AsString;
         ResultSongs.Add(song);
         
-		QueryResults.Next;
+        QueryResults.Next;
       end;
     end;
   finally
@@ -621,6 +717,176 @@ end;
 function TJukeboxDB.SqlWhereClause: String;
 begin
   SqlWhereClause := ' WHERE encrypted = 0';
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.InsertSong(Song: TSongMetadata): Boolean;
+var
+  InsertSuccess: Boolean;
+begin
+  InsertSuccess := false;
+
+  //TODO: implement InsertSong
+
+  InsertSong := InsertSuccess;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.UpdateSong(Song: TSongMetadata): Boolean;
+var
+  UpdateSuccess: Boolean;
+begin
+  UpdateSuccess := false;
+
+  //TODO: implement UpdateSong
+
+  UpdateSong := UpdateSuccess;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.StoreSongMetadata(Song: TSongMetadata): Boolean;
+var
+  DbSong: TSongMetadata;
+  Success: Boolean;
+begin
+  Success := false;
+
+  DbSong := RetrieveSong(Song.Fm.FileUid);
+  if DbSong <> nil then begin
+    //TODO: implement StoreSongMetadata
+    {
+    if Song <> DbSong then begin
+      Success := UpdateSong(Song);
+    end
+    else begin
+      Success := true;  // no insert or update needed (already up-to-date)
+    end;
+    }
+  end
+  else begin
+    // song is not in the database, insert it
+    Success := InsertSong(Song);
+  end;
+
+  StoreSongMetadata := Success;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.InsertPlaylist(PlUid: String;
+                                   PlName: String;
+                                   PlDesc: String): Boolean;
+var
+  InsertSuccess: Boolean;
+  Sql: String;
+begin
+  InsertSuccess := false;
+
+  if (DbConnection <> nil) and
+     (PlUid.Length > 0) and
+     (PlName.Length > 0) then begin
+
+    //TODO: implement InsertPlaylist
+    {
+    if not BeginTransaction then begin
+      InsertPlaylist := false;
+      exit;
+    end;
+
+    Sql := 'INSERT INTO playlist VALUES (:pl_uid,:pl_name,:pl_desc)';
+
+    const Args = new PropertyList;
+    Args.Append(new PropertyValue(PlUid));
+    Args.Append(new PropertyValue(PlName));
+    Args.Append(new PropertyValue(PlDesc));
+
+    const RowsAffected = DbConnection.Execute(SqlStatement, Args);
+    if RowsAffected = 0 then begin
+      Rollback;
+    end
+    else begin
+      InsertSuccess := Commit;
+    end;
+    }
+  end;
+
+  InsertPlaylist := InsertSuccess;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.DeleteSong(SongUid: String): Boolean;
+var
+  WasDeleted: Boolean;
+  Sql: String;
+begin
+  WasDeleted := false;
+  if DbConnection <> nil then begin
+    if SongUid.Length > 0 then begin
+      if not BeginTransaction then begin
+        writeLn('error: begin transaction failed');
+        DeleteSong := false;
+        exit;
+      end;
+
+      //TODO: implement DeleteSong
+      {
+      const ArgList = new PropertyList;
+      ArgList.Append(new PropertyValue(SongUid));
+
+      Sql := 'DELETE FROM song WHERE song_uid = :song_uid';
+      const RowsAffected = DbConnection.Execute(SqlStatement, SongUid);
+
+      if RowsAffected = 0 then begin
+        Rollback;
+        writeLn('error: unable to delete song ' + SongUid);
+      end
+      else begin
+        WasDeleted := Commit;
+      end;
+      }
+    end;
+  end;
+
+  DeleteSong := WasDeleted;
+end;
+
+//*******************************************************************************
+
+function TJukeboxDB.DeletePlaylist(PlName: String): Boolean;
+var
+  DeleteSuccess: Boolean;
+  Sql: String;
+begin
+  DeleteSuccess := false;
+
+  if (DbConnection <> nil) and (PlName.Length > 0) then begin
+    if not BeginTransaction then begin
+      DeletePlaylist := false;
+      exit;
+    end;
+
+    Sql := 'DELETE FROM playlist WHERE playlist_name = :playlist_name';
+
+    //TODO: implement DeletePlaylist
+    {
+    const Args = new PropertyList;
+    Args.Append(new PropertyValue(PlName));
+
+    const RowsAffected = DbConnection.Execute(SqlQuery, Args);
+    if RowsAffected = 0 then begin
+      Rollback;
+    end
+    else begin
+      DeleteSuccess := Commit;
+    end;
+    }
+  end;
+
+  DeletePlaylist := DeleteSuccess;
 end;
 
 //*******************************************************************************
