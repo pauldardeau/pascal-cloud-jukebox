96b79a5660f3cde09facac7c0aef55a857eccffa
Paul Dardeau <pauldardeau@gmail.com>
2023-05-15 16:32:27 -0500

    redirect stdout and stderr on some batch files

--- scripts/s3-get-object.bat
+++ scripts/s3-get-object.bat
@@ -3,4 +3,4 @@ set BUCKET_NAME="%%BUCKET_NAME%%"
 set OBJECT_NAME="%%OBJECT_NAME%%"
 set S3_ENDPOINT_URL="%%S3_ENDPOINT_URL%%"
 
-aws s3 cp --endpoint-url=%S3_ENDPOINT_URL% s3://%BUCKET_NAME%/%OBJECT_NAME% %%OUTPUT_FILE%%
+aws s3 cp --endpoint-url=%S3_ENDPOINT_URL% s3://%BUCKET_NAME%/%OBJECT_NAME% %%OUTPUT_FILE%% 1>stdout.txt 2>stderr.txt
--- scripts/s3-list-containers.bat
+++ scripts/s3-list-containers.bat
@@ -1,3 +1,3 @@
 @echo off
 set S3_ENDPOINT_URL="%%S3_ENDPOINT_URL%%"
-aws s3 ls --endpoint-url=%S3_ENDPOINT_URL%
+aws s3 ls --endpoint-url=%S3_ENDPOINT_URL% 1>stdout.txt 2>stderr.txt
